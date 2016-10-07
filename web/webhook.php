56df0372ea0c6e07610b862b9342ed715c7c7bb3
Konr Ness <kness@nerdery.com>
2016-10-07 16:07:25 -0500

    Initial commit

---
+++ web/webhook.php
@@ -0,0 +1,49 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$config = include '../app/config.php';
+
+$client_token = $_GET['token'] ?: null;
+$client_ip    = $_SERVER['REMOTE_ADDR'];
+
+$log = fopen('../log/webhook.log', 'a');
+fwrite($log, '['.date("Y-m-d H:i:s").'] ['.$client_ip.']'.PHP_EOL);
+
+// verify token
+if ($client_token !== $config['access_token'])
+{
+    echo "invalid token";
+    fwrite($log, "Invalid token [{$client_token}]".PHP_EOL);
+    exit(0);
+}
+
+// verify IP
+if ($client_ip !== $config['client_ip'])
+{
+    echo "invalid ip";
+    fwrite($log, "Invalid ip [{$client_ip}]".PHP_EOL);
+    exit(0);
+}
+
+$event = $_SERVER['HTTP_X_GITLAB_EVENT'];
+fwrite($log, "Event Type: $event\n");
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+fwrite($log, "Event Data: $json\n");
+
+
+switch ($event) {
+    case "Merge Request Hook":
+        if ($data['object_attributes']['action'] == 'open') {
+            // @todo Create bamboo plan branch
+        }
+
+        break;
+    case "Note Hook":
+
+
+        break;
+}
